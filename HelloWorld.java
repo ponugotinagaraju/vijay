a9803352a0c17724ce419776a197327e1d6d2d68
ponugotinagaraju <36818264+ponugotinagaraju@users.noreply.github.com>
2018-03-20 03:57:15 -0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,8 @@ public class HelloWorld{
 	
 	public static void main(String []a){
 		System.out.println("hello world");
+		if(a[0].equals("naga")){
+			System.out.println("hello welocome:"+a[0]);
+		}
 	}
 }
